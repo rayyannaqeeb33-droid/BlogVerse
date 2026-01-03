4f3bff980a70b67cfdd77b48a4e1f1fc8d000315
rayyannaqeeb33-droid <rayyannaqeeb33@gmail.com>
2026-01-03 19:23:25 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCASpom_DJMQf2stSGz4dGRYx0P6cTHNPs",
+  authDomain: "blogverse-407c3.firebaseapp.com",
+  projectId: "blogverse-407c3",
+  storageBucket: "blogverse-407c3.firebasestorage.app",
+  messagingSenderId: "910306120926",
+  appId: "1:910306120926:web:fe48dad901b57144cd5e4f",
+  measurementId: "G-WXWRCT2L74"
+};
